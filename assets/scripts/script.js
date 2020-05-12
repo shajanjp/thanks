89058d8dae5a8e072a1fe232a93601cc38f16155
shajanjp <shajanjp@gmail.com>
2020-05-12 09:15:52 +0530

    fix: adds logic for thanks carousal

---
+++ assets/scripts/script.js
@@ -0,0 +1,41 @@
+---
+---
+
+const thanksContainer = document.getElementsByClassName('thanks-container')[0];
+
+function getThanksData() {
+  return fetch('{{site.baseurl}}/data/thanks.json')
+  .then(res => res.json())
+  .catch(error =>{
+
+  })
+}
+
+function makeThanks(thanksData){
+  return `<div class="thanks animate__animated animate__fadeInUp">
+  <p class="for">${thanksData.for}</p>
+  <p class="to-whom">
+  <span class="to">${thanksData.to}</span> - <span class="who">${thanksData.who}</span>
+  </p>
+  </div>`;
+}
+
+function startThanking(thanksData){
+  let thanksCounter = 0;
+  setInterval(() => {
+    thanksContainer.innerHTML = makeThanks(thanksData[thanksCounter]);
+
+    if(thanksCounter == thanksData.length - 1){
+      thanksCounter = 0;
+    }
+    else{
+      thanksCounter++;
+    }
+  }, 3500)
+}
+
+
+getThanksData()
+.then(data => {
+  startThanking(data);
+})
